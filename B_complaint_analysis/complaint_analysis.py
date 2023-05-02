ab373ddc00189065ae9d8d903b578c104e67a218
120090049 <120090049@link.cuhk.edu.cn>
2023-05-02 14:51:41 +0800

    clp

--- B_complaint_analysis/complaint_analysis.py
+++ B_complaint_analysis/complaint_analysis.py
@@ -7,10 +7,11 @@ parent_dir = os.path.dirname(current_dir)
 
 file_path = os.path.join(parent_dir, 'data', 'complaint_score.xlsx')
 
-def complaint_analysis():
+def complaint_analysis(stall_index_dict, stall_canteen_dict):
 
     table = openpyxl.load_workbook(file_path).active
-    
+    stall_score_list = [5] * 45
+    first_row = True
     for row_index in range(1,table.max_row+1):
         
         canteen = table.cell(row_index, 1).value
@@ -21,11 +22,34 @@ def complaint_analysis():
             first_row = False
             continue
         else:
-            # 预测得分
-            pass
+            break
 
-    return 
+    return stall_score_list
 
 if __name__ == "__main__":
-    final_list = complaint_analysis()
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    file_path = os.path.join(parent_dir, 'data', 'stall_rating.xlsx')
+
+    # 读取Excel文件
+    table = openpyxl.load_workbook(file_path)
+    table_sheet = table.active
+
+    first_row = True
+    
+    stall_index_dict = {} # {stall_name: index}
+    stall_canteen_dict = {} #  {stall_name: canteen}
+    for row_index in range(1,table_sheet.max_row+1):
+        # 读取第一列和第二列的值
+        canteen = table_sheet.cell(row_index, 1).value
+        stall = table_sheet.cell(row_index, 2).value
+
+        if (first_row):
+            first_row = False
+            continue
+        else:
+            stall_index_dict[stall] = row_index-1
+            stall_canteen_dict[stall] = canteen
+            
+    final_list = complaint_analysis(stall_index_dict, stall_canteen_dict)
     print(final_list)
--- main.py
+++ main.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
 
     first_row = True
     
-    stall_index_dict = {}
-    stall_canteen_dict = {}
+    stall_index_dict = {} # {stall_name: index}
+    stall_canteen_dict = {} #  {stall_name: canteen}
     for row_index in range(1,table_sheet.max_row+1):
         # 读取第一列和第二列的值
         canteen = table_sheet.cell(row_index, 1).value
@@ -29,11 +29,7 @@ if __name__ == "__main__":
             stall_index_dict[stall] = row_index-1
             stall_canteen_dict[stall] = canteen
     
-    # print(stall_dict)
-    # stall_index_dict    {stall_name: index}
-    # stall_canteen_dict  {stall_name: canteen}
-    print(stall_index_dict)
-    print(stall_canteen_dict)
+
     # list_A = bayesian_ranking()
-    # list_B = complaint_analysis(first_row)
-    # print(final_list)
+    list_B = complaint_analysis(stall_index_dict, stall_canteen_dict)
+    print(list_B)
